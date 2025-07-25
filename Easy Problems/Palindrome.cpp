f6bf7e0588cb8b0a85aee6f66dcdb961535246c1
Swagat2 <Shreemanswagat2002@gmail.com>
2025-07-25 21:25:16 +0530

    added

---
+++ Easy Problems/Palindrome.cpp
@@ -0,0 +1,26 @@
+//Check if a number is Palindrome
+// Given an integer n, find whether the number is Palindrome or not. A number is a Palindrome if it remains the same when its digits are reversed.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a digit : ";
+    cin>>n;
+    int original=n;
+    int rev=0;
+    for (;n !=0; n/=10)
+    {
+        int rem=n%10;
+        rev=rev*10+rem;
+    }
+    if (rev==original)
+    {
+        cout<<"Palindrome";
+    }
+    else{
+        cout<<"Not palindrome";
+    }
+    
+    
+}
